3665e39df55fc72fc7897156d350bda08179a0e5
Daniel Hartwig <mandyke@gmail.com>
2012-03-09 22:54:33 +0800

    Revert recent changes which messed up the tree
    Reverted commits:
    * dd99f3933f0de710ef00bc83385d3161dec3e987
    * 5e0bc67d458d912859148eb07a1dafef93ae68d8
    * ef8fdce57c43345c26b6c2df797b1600aee50186

--- doc/po4a/add_pl/addendum.1.pl
+++ doc/po4a/add_pl/addendum.1.pl
@@ -1,19 +1,6 @@
 PO4A-HEADER:mode=after;position=</author>;beginboundary=<legalnotice>
     <othercredit>
       <personname>
-<<<<<<< HEAD
-        <firstname>MichaÅ</firstname> <surname>KuÅach</surname>
-      </personname>
-      <email>michal.kulach@gmail.com</email>
-      <contrib>TÅumaczenie</contrib>
-    </othercredit>
-    <othercredit>
-      <personname>
-        <firstname>MichaÅ</firstname> <surname>Politkowski</surname>
-      </personname>
-      <email>l10n@mp.pl.eu.org</email>
-      <contrib>TÅumaczenie</contrib>
-=======
         <firstname>Michał</firstname> <surname>Kułach</surname>
       </personname>
       <email>michal.kulach@gmail.com</email>
@@ -25,5 +12,4 @@ PO4A-HEADER:mode=after;position=</author>;beginboundary=<legalnotice>
       </personname>
       <email>l10n@mp.pl.eu.org</email>
       <contrib>Tłumaczenie</contrib>
->>>>>>> 647f2d6f828cb66a0fc0225f197677264b63adbe
     </othercredit>
